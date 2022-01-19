76109b0526f81dc703c88327918f721f0a8a9630
ZJUSTKIDDING <64264268+ZJUSTKIDDING@users.noreply.github.com>
2022-01-19 13:57:27 +0200

    Initialize interactives-randomize-questions

---
+++ ozaria/site/components/interactive/PageInteractive/common/InteractiveTitle.vue
@@ -0,0 +1,45 @@
+<script>
+  import { mapGetters } from 'vuex'
+
+  import { internationalizeConfig } from '../../../../common/ozariaUtils'
+
+  export default {
+    props: {
+      interactive: {
+        type: Object,
+        required: true
+      }
+    },
+
+    computed: {
+      ...mapGetters({
+        userLocale: 'me/preferredLocale'
+      }),
+
+      localizedInteractive () {
+        return internationalizeConfig(this.interactive, this.userLocale)
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="title-container">
+    {{ localizedInteractive.promptText }}
+  </div>
+</template>
+
+<style scoped>
+  .title-container {
+    padding: 30px;
+
+    width: 100%;
+
+    border: 1px solid black;
+
+    text-align: center;
+
+    font-size: 30px;
+    line-height: 30px;
+  }
+</style>
